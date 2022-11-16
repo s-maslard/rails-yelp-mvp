d3c8bf841b6f03011cfa21ae05c1e7f591ffd1eb
sylvain maslard <sylv5@hotmail.fr>
2022-11-16 15:18:45 +0100

    Yelp MVP

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_16_124652) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_16_140025) do
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "address"
     t.string "phone_number"
     t.string "category"
+    t.string "reviews"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "reviews"
   end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.string "content"
+    t.integer "restaurant_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["restaurant_id"], name: "index_reviews_on_restaurant_id"
+  end
+
+  add_foreign_key "reviews", "restaurants"
 end
